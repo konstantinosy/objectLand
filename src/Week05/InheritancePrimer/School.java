b4adef6e107bf3dada1264d29f7fe87665cd9481
konstantinosy <kgiasemis@nyc.gr>
2024-03-12 03:05:30 +0200

    Wrote 4 lines into 2.

--- src/Week05/InheritancePrimer/School.java
+++ src/Week05/InheritancePrimer/School.java
@@ -4,13 +4,12 @@ package Week05.InheritancePrimer;
 
 public class School {
     public static void main(String[] args) {
-        Student s = new Student(1, "Mike",
-                "mike@school.test", "Computing");
-        Instructor i = new Instructor(2, "John",
-                "john@instr.school.test");
+        Student s = new Student(1, "Mike", "mike@school.test", "Computing");
+        Instructor i = new Instructor(2, "John", "john@instr.school.test");
 
         Person p = s;
         System.out.println(p);
+
         p = i;
         System.out.println(p);
     }
